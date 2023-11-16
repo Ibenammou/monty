81fd446199b247b3e36259440c5b270eadb28ffd
Ibenammou <imanebenamou@gmail.com>
2023-11-16 15:51:48 -0800

    task0 done

--- main_monty.c
+++ main_monty.c
@@ -6,37 +6,51 @@
  * @argc: number of arguments.
  * @argv: array of args.
  * Return: number.
- */// ...
-
+ */
 int main(int argc, char **argv)
 {
-    char line[100], *_token;
-    int i = 0;
-    allData.stack_mode = 0;
-
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s file\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    allData.my_file = _open(argv[1]);
-
-    while (fgets(line, sizeof(line), allData.my_file) != NULL)
-    {
-        // ...
-
-        if (strcmp(allData.opcode, "push") == 0 && i == 1)
-        {
-            fprintf(stderr, "L%d: Usage: push integer\n", allData.line_num);
-            free_function(allData.stack_head);
-            exit(EXIT_FAILURE);
-        }
-
-        execute_opcode();
-    }
-
-    free_function(allData.stack_head);
-    exit(EXIT_SUCCESS);
+char arguments[100], *_token;
+int i = 0;
+allData.stack_mode = 0;
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
 }
-
+allData.my_file = _open(argv[1]);
+while (fgets(arguments, sizeof(arguments), allData.my_file) != NULL)
+{
+if (strlen(delete_spaces(arguments)) < 3 || delete_spaces(arguments)[0] == '#')
+{
+allData.line_num++;
+continue;
+}
+allData.line_num++;
+_token = strtok(arguments, " \n");
+for (i = 0; _token != NULL && i < 2; i++)
+{
+if (i == 0)
+{
+allData.opcode = _token;
+}
+if (strcmp(allData.opcode, "push") != 0)
+{
+break;
+}
+if (i == 1)
+{
+check(_token);
+}
+_token = strtok(NULL, " \n");
+}
+if (strcmp(allData.opcode, "push") == 0 && i == 1)
+{
+fprintf(stderr, "L%d: usage: push integer\n", allData.line_num);
+free_function(allData.stack_head);
+exit(EXIT_FAILURE);
+}
+execute_opcode();
+}
+free_function(allData.stack_head);
+exit(EXIT_SUCCESS);
+}
